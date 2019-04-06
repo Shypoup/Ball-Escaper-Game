52c35ef92c2e53e3d1ba43b73d86c3a5507fec16
Shypoup <hesham.fcih@gmail.com>
2019-04-06 21:11:31 +0200

    Level 1

---
+++ Assets/CameraPosition.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+public class CameraPosition : MonoBehaviour {
+	public Transform player;
+	public Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.position + offset;
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Rigidbody rg;
+	public float forwardForce;
+	public float sidewaysForce;
+	public KeyCode leftButton, rightButton, forwardButton,slowButton;
+    public Text scoreText;
+    public int score = 0;
+    
+
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		rg.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey(forwardButton))
+        {
+            forwardForce += sidewaysForce * Time.deltaTime;
+        }
+        if (Input.GetKey(slowButton))
+        {
+            forwardForce -= sidewaysForce * Time.deltaTime;
+        }
+
+        if (Input.GetKey(rightButton))
+		{
+			rg.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+
+
+        if (Input.GetKey(leftButton))
+		{
+			rg.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+
+		
+
+
+
+
+
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        
+            Destroy(other.gameObject);
+             score+= 1;
+        scoreText.text = score.ToString();
+        
+    }
+}
